b5840664a43b3df8eefb07e367ecd80211583179
mikea <mikeaav@msn.com>
2019-05-24 23:05:42 -0400

    lesson2

---
+++ src/Lesson2.java
@@ -0,0 +1,26 @@
+public class Lesson2
+{
+    public static void main(String[] args) {
+        int numbera = 1;
+        int numberb = 5;
+        int total;
+        total = numbera + numberb;
+        System.out.println(total);
+
+        double power = 2;
+        double base = 5;
+        double result;
+
+        result = Math.pow(base, power);
+        System.out.println(result);
+
+        int a = 5;
+        double b = 6.0;
+        int product = a* (int)b;
+        double productWithDecimal = (double)a * b;
+        System.out.println(product);
+        System.out.println(productWithDecimal);
+
+
+    }
+}
